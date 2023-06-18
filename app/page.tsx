9e600f4a652d8546d530b22d77f9f70e3485a829
Matvey <morozovmatvej75@gmail.com>
2023-06-18 18:05:18 +0300

    add basic homepage & typography for articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <h1>Blog website</h1>
+      <h2>
+        Made with{" "}
+        <Link href="https://nextjs.org">
+          <span className="font-bold">Next.JS</span>
+        </Link>
+      </h2>
+    </>
+  );
 }
