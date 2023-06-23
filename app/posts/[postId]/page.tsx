b8ea0beaf522f151739c0774b9e52b8287b27998
Matvey <morozovmatvej75@gmail.com>
2023-06-23 19:31:02 +0300

    fix css classes alignment

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -33,34 +33,34 @@ export default async function Post({ params: { postId } }: PostParams) {
       <p className="font-light text-gray-600 dark:text-gray-400">{post.date}</p>
       <article
         className="
-        prose
+          prose
 
-      text-gray-600
-      prose-headings:font-bold
-      prose-headings:text-black
-      prose-p:text-gray-600
-      prose-a:font-semibold
-      prose-a:text-gray-900
+          text-gray-600
+          prose-headings:font-bold
+          prose-headings:text-black
+          prose-p:text-gray-600
+          prose-a:font-semibold
+          prose-a:text-gray-900
 
-      prose-blockquote:text-gray-800
-      prose-code:text-black
-      prose-pre:border
-      prose-pre:border-gray-300
-      prose-pre:bg-gray-200
-      dark:text-gray-400
-      dark:prose-headings:text-white
-      dark:prose-p:text-gray-400
-      dark:prose-a:text-gray-200
-      dark:prose-blockquote:border-gray-500
-      dark:prose-blockquote:text-gray-400
-      dark:prose-figcaption:text-gray-300
-      dark:prose-strong:text-gray-200
-      dark:prose-code:text-white
-      dark:prose-pre:border-gray-600
-        dark:prose-pre:bg-gray-700
-        dark:prose-thead:border-y-gray-500
-        dark:prose-tr:border-y-gray-600
-        dark:prose-hr:border-gray-600
+          prose-blockquote:text-gray-800
+          prose-code:text-black
+          prose-pre:border
+          prose-pre:border-gray-300
+          prose-pre:bg-gray-200
+          dark:text-gray-400
+          dark:prose-headings:text-white
+          dark:prose-p:text-gray-400
+          dark:prose-a:text-gray-200
+          dark:prose-blockquote:border-gray-500
+          dark:prose-blockquote:text-gray-400
+          dark:prose-figcaption:text-gray-300
+          dark:prose-strong:text-gray-200
+          dark:prose-code:text-white
+          dark:prose-pre:border-gray-600
+          dark:prose-pre:bg-gray-700
+          dark:prose-thead:border-y-gray-500
+          dark:prose-tr:border-y-gray-600
+          dark:prose-hr:border-gray-600
         "
       >
         <h1>{post.title}</h1>
