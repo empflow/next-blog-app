04d4842bb30aa4f7ca098245ecbb7c2d37e0aced
Matvey <morozovmatvej75@gmail.com>
2023-06-23 19:47:45 +0300

    improve post not found page
    made the text larger & added back to posts button

--- app/posts/[postId]/not-found.tsx
+++ app/posts/[postId]/not-found.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function NotFound() {
-  return <h1>The post you&apos;re looking for doesn&apos;t exist!</h1>;
+  return (
+    <>
+      <h1 className="mb-4 text-xl">
+        The post you&apos;re looking for doesn&apos;t exist!
+      </h1>
+      <Link href="/posts">
+        <button className="rounded bg-blue-600 px-3 py-1 text-sm text-gray-50 dark:bg-blue-700">
+          Back to all posts
+        </button>
+      </Link>
+    </>
+  );
 }
