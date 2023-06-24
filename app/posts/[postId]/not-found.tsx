567fad0716aedb055367a9fe5f2c84056e9dc85c
Matvey <morozovmatvej75@gmail.com>
2023-06-24 11:46:57 +0300

    remove unused metadata object

--- app/posts/[postId]/not-found.tsx
+++ app/posts/[postId]/not-found.tsx
@@ -1,9 +1,4 @@
 import Link from "next/link";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Post not found",
-};
 
 export default function NotFound() {
   return (
