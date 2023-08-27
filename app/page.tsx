c6c6d7203399cffcea1a329876d2187afa7a0bea
Matvey <empflow@tutanota.com>
2023-08-27 16:48:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <section className="mb-10">
-        <h1 className="mb-2 text-3xl font-semibold">Что я прочитал летом</h1>
+        <h1 className="mb-2 text-3xl font-semibold">Что я не прочитал летом</h1>
       </section>
 
       <section>
